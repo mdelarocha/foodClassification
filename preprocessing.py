c9b6c74432372c496d38bdbd1c4e72b28826a4f8
Miguel de la Rocha <delarochamiguelalex@gmail.com>
2018-04-22 13:21:03 -0500

    Updated some Settings
    Multiline=False since it is a JSON file, removed some of the show statements, and set it so we can do other changes along the way.

--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,24 @@
 #Preprocess
 
-#Import libraries needed 
+# Import libraries needed 
+
 from sparkdl import readImages
 from pyspark.sql.functions import lit
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import col
 from pyspark.sql.types import StructType,StructField, LongType
 
-#Find distinct labels
-df=spark.read.json("/FileStore/tables/photos.json", multiLine=True)
-df.show()
-df.select("label").show()
+# Find distinct labels
+
+df=spark.read.json("/FileStore/tables/photos.json", multiLine=False)
 df.groupBy("label").count().show()
 df.select("label").distinct().count()
 df.select("label").distinct().show()
 
-#Image dataframe 
-img_dir='/FileStore/tables/'
-images_df=readImages(img_dir+"jpg").withColumn("label", lit(1))
-images_train, images_test=images_df.randomSplit([0.6,0.4])
+# Image dataframe
+
+img_dir='/FileStore/tables/yelp_photos'
+images_df=readImages(img_dir)
+
+# Wait until we map and normalize the images
+# images_train, images_test=images_df.randomSplit([0.7,0.3]) 
